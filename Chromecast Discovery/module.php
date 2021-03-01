20f606a5e4d7f0a3266b88564f30fe856b220fd8
stigkristiansen <stigkristiansen@outlook.com>
2021-03-01 17:58:21 +0100

    .

--- Chromecast Discovery/module.php
+++ Chromecast Discovery/module.php
@@ -11,7 +11,7 @@
 		public function __construct($InstanceID) {
 			parent::__construct($InstanceID);
 	
-			$this->dnsSdId = GetDnsSdId();
+			$this->dnsSdId = $this->GetDnsSdId();
 		}
 
 		public function Create()

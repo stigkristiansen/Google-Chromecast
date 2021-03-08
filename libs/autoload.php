edb846c50a2aac2e5b77e634b1f15c1520c19a0e
stigkristiansen <stigkristiansen@outlook.com>
2021-03-08 19:43:28 +0100

    .

--- libs/autoload.php
+++ libs/autoload.php
@@ -3,8 +3,12 @@ declare(strict_types=1);
 
 include_once __DIR__ . '/traits.php';
 
+spl_autoload_extensions(".php"); // comma-separated list
+    spl_autoload_register();
+
 foreach (glob(__DIR__ . '/*.php') as $filename) {
     if (basename($filename) != 'autoload.php') {
         include_once $filename;
     }
 }
+

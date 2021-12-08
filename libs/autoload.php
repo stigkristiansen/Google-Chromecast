99b4e777cd43c2321570daf7699afd2a95c4a8fa
stigkristiansen <stigkristiansen@outlook.com>
2021-12-08 23:32:01 +0100

    Update autoload.php

--- libs/autoload.php
+++ libs/autoload.php
@@ -3,13 +3,6 @@ declare(strict_types=1);
 
 include_once __DIR__ . '/traits.php';
 
-function CustomAutoload($ClassName) {
-    //IPS_LogMessage('Autoload', 'Class: '.$ClassName);
-    include(__DIR__ . "/" . $ClassName . ".php");
-}
-
-spl_autoload_register("CustomAutoload");
-
 foreach (glob(__DIR__ . '/*.php') as $filename) {
     if (basename($filename) != 'autoload.php') {
         include_once $filename;
